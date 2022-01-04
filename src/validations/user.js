6222c52fc800b8982e9a5504454583766eeac764
iangYo <iangabrielrodrigues@gmail.com>
2022-01-04 20:37:30 -0300

    feat: Specific validation to login

--- src/validations/user.js
+++ src/validations/user.js
@@ -18,4 +18,18 @@ const create = Joi.object().keys({
   })
 });
 
-module.exports = { create, login: create };
+const login = Joi.object().keys({
+  email: Joi.string().email().required().messages({
+    'string.base': 'Email deve ser do tipo texto',
+    'string.email': 'Email deve ser válido',
+    'any.required': 'Email é obrigatório'
+  }),
+  password: Joi.string().min(8).max(16).required().messages({
+    'string.base': 'Senha deve ser do tipo texto',
+    'any.required': 'Senha é obrigatório',
+    'string.min': 'Senha deve ter ao menos 8 caracteres',
+    'string.max': 'Senha deve ter no máximo 16 caracteres!'
+  })
+});
+
+module.exports = { create, login };
