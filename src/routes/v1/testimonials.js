a5315939d5952a47455f77965c0a4412d1cced7a
iangYo <iangabrielrodrigues@gmail.com>
2022-01-05 12:25:34 -0300

    feat: Checks the integrity of the token before reach controller

--- src/routes/v1/testimonials.js
+++ src/routes/v1/testimonials.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 
 const serviceLocator = require('../../serviceLocator/register');
 const validator = require('../../middleware/validator');
+const validateToken = require('../../middleware/token');
 const testimonial = require('../../validations/testimonial');
 
 // router.post('/login', validator(userSchema.login), (req, res, next) => {
 //   serviceLocator.get('authController').login(req, res, next);
 // });
 
-router.post('/', validator(testimonial.create), (req, res) => {
+router.post('/', validateToken, validator(testimonial.create), (req, res) => {
   serviceLocator.get('testimonialController').create(req, res);
 });
 
